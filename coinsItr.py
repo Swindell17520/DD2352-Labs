c956512ce15d8f7ea3c4231159aa27ba2b24b9c9
Swindell17520 <59296149+Swindell17520@users.noreply.github.com>
2022-01-27 15:22:47 +0100

    Update coinsItr.py

--- coinsItr.py
+++ coinsItr.py
@@ -4,7 +4,6 @@ Created on Thu Jan 27 14:34:57 2022
 
 @author: Rahul
 """
-
 import sys
 n = int(input())
 a = int(input())
@@ -15,17 +14,17 @@ def min(x, y):
     return x
   return y
 
-def coin_change(d, n, k):
+def coin_change(f, n, k):
   M = [0]*(n+1)
 
   for j in range(1, n+1):
-    minimum = float('inf')
+    mini = float('inf')
 
     for i in range(1, k+1):
-      if(j >= d[i]):
-        minimum = min(minimum, 1+M[j-d[i]])
-    M[j] = minimum
+      if(j >= f[i]):
+        mini = min(mini, 1+M[j-f[i]])
+    M[j] = mini
   return M[n]
 
-d = [0,a,b,c]
-print(coin_change(d, n, 3))
+f = [0,a,b,c]
+print(coin_change(f, n, 3))
