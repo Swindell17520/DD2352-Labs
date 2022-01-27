65dab9f60f0023ddc505532e38eacf488c0ab61f
Swindell17520 <59296149+Swindell17520@users.noreply.github.com>
2022-01-27 16:40:26 +0100

    Create winstreakDP.py

---
+++ winstreakDP.py
@@ -0,0 +1,20 @@
+import sys
+import timeit
+sys.setrecursionlimit(100000)
+n = int(input())
+k = int(input())
+p = float(input())
+dp = {}
+def Win_Streak(n,k):
+    if n and k in dp:
+        return dp[n][k]
+    elif k == 0:
+        return 1.0
+    elif n == 0 and k > 0:
+        return 0.0
+    else:
+        dp[n][k] = p*Win_Streak(n-1,k-1) + (1-p)*Win_Streak(n-1,k)
+        return dp[n]
+
+
+print(Win_Streak(n,k))
